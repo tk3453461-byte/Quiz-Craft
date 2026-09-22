09208fa19db36c2c27eefecb7a5ccb2c1173b131
tk3453461-byte <tk3453461@gmail.com>
2026-09-22 21:46:45 +0530

    Add text cleaning and PDF extraction functions

---
+++ text_processor.py
@@ -0,0 +1,24 @@
+import re 
+from PyPDF2 import PdfReader
+
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+def extract_text_from_pdf(pdf_file):
+    reader = PdfReader(pdf_file)
+
+    text =  ""
+
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    return text
+# tasting
+sample_text = """
+python   is a programming language.
+
+it is easy    to learn.
+"""
+cleaned_text = clean_text(sample_text)
+print(cleaned_text)
